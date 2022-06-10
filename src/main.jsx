df7e8561b78a716c8fb26aa7476c26e2ed658467
Thiago Moreira <thiagomoreira.ifsc@gmail.com>
2022-06-10 18:00:57 -0300

    inicializando estrutura de pastas, definido cores do tema

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+import GlobalStyles from './styles/global'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+
+    </ThemeProvider>
+  </React.StrictMode>
+)
---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+export default{
+    COLORS: {
+        BACKGROUND_900: "#1C1B1E",
+        BACKGROUND_800: "#262529",
+        BACKGROUND_700: "#312E38",
+        
+        GRAY_400: "#3E3B47",
+        GRAY_300: "#999591",
+        GRAY_200: "#948F99",
+        GRAY_100: "#E5E5E5",
+
+        LIGHT_GRAY: "#F4EDE8",
+        WHITE: "FFFFFF",
+        
+        ROSE: "#FF859B",
+        DARK_ROSE: "#FF859B0d",
+    }
+}
